101414c6a29475c390682f2e0e8abac07ee5e043
‪Oleg Braylovsky‬‏ <92874809+OlegBray@users.noreply.github.com>
2022-05-09 10:49:51 +0300

    First Update
    Implementing some super flat application plan without logic currently.

---
+++ RecepieToShoping/src/app/app.component.spec.ts
@@ -0,0 +1,31 @@
+import { TestBed } from '@angular/core/testing';
+import { AppComponent } from './app.component';
+
+describe('AppComponent', () => {
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [
+        AppComponent
+      ],
+    }).compileComponents();
+  });
+
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+  it(`should have as title 'RecepieToShoping'`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app).toEqual('RecepieToShoping');
+  });
+
+  it('should render title', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('.content span')?.textContent).toContain('RecepieToShoping app is running!');
+  });
+});
---
+++ RecepieToShoping/src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: "app-recipe-detail",
+    styleUrls: ['./recipe-detail.component.scss'],
+    templateUrl: './recipe-detail.component.html'
+})
+export class RecipeDetail{
+    constructor(){  
+    }
+}
